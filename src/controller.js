223eecd0082003e90b608763ae12c64c1ee578c6
Cyberdesu <78768764+Suredsi@users.noreply.github.com>
2022-10-01 17:26:42 +0700

    Create controller.js

---
+++ src/controller.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const { response } = require('express');
+const con = require('./Database/database')
+
+const addfingerprint = (req, res) => {
+    const sql = "INSERT INTO `siswa` (`id`, `nama`, `kelas`, `jenis kelamin`, `no_ortu`) VALUES (NULL, NULL, NULL, NULL, NULL)"
+
+    con.query(sql,function(err,result,fields){
+        if (err) throw err;
+        res.status(200)
+        res.send({
+            success: 201,
+            message: "id telah ditambahkan",
+            data: result.insertId
+        })
+    })
+}
+module.exports = {addfingerprint}
