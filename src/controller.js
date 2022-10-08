66260e8ac3f550a16164ecdf47ae9d91cd64cf56
Cyberdesu <78768764+Suredsi@users.noreply.github.com>
2022-10-09 06:00:42 +0700

    Update controller.js

--- src/controller.js
+++ src/controller.js
@@ -38,13 +38,44 @@ const deletefingerprint = (req, res) => {
                 message: "id siswa sudah dihapus atau kosong"
             })
         } else{
+            res.status(500)
             res.send({
                 success: true,
                 message: "id siswa telah dihapus"
             })
         }
     })
+    
+}
+
+const getDatasiswa = (req,res) => {
+    const sql = "SELECT * FROM siswa"
+    con.query(sql,function(err,result){
+        if (err) throw err;
+        res.status(200)
+        res.send({
+            success: true,
+            data: result
+        })
+    })
+}
+
+const DeviceMode = (req, res) => {
+    let id = req.params.id
+    let mode = req.params.mode
+    const sql = "SELECT * FROM device WHERE device_id=? "
+    if(id = !undefined && mode == "daftar"){
+        con.query(sql,[id],function(err,result){
+            if (err) throw err;
+            res.send({
+                success:true,
+                data: result
+            })
+        })
+    } else if (id == 0 && mode == "baru"){
+
+    }
 }
 
 
-module.exports = {addfingerprint,home,deletefingerprint}
+module.exports = {addfingerprint,home,deletefingerprint,getDatasiswa,DeviceMode}
