3f70ad4ecc1c6c4b7125da64c5048bac775ba223
Cyberdesu <78768764+Suredsi@users.noreply.github.com>
2022-10-11 13:07:12 +0700

    Update database.js

--- src/Database/database.js
+++ src/Database/database.js
@@ -1,10 +1,10 @@
 /* eslint-disable linebreak-style */
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'absensi',
+  host: 'us-cdbr-east-06.cleardb.net',
+  user: 'be5dddd2b057a9',
+  password: 'a8b7ee32',
+  database: 'heroku_19be3f159ae3ff9',
 });
 connection.connect(function (err){
   if(err) throw err;
