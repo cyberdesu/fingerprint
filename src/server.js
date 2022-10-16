d7df21cce98fb92fad9967b24ef3d43d11c94151
Cyberdesu <78768764+Suredsi@users.noreply.github.com>
2022-10-16 07:16:39 +0700

    update modules

--- src/server.js
+++ src/server.js
@@ -5,12 +5,19 @@ const port = process.env.PORT || 4000
 const cors = require ('cors')
 const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
 
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
 
 const appRoute = require('./router');
 app.use('/', appRoute)
+
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
