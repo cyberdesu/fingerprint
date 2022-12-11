672618011b35ec7e8bc8d217b69d306ef7ef3afb
Cyberdesu <78768764+Suredsi@users.noreply.github.com>
2022-12-11 14:48:42 +0700

    migrasi ke datatables

--- src/controller/DataAbsen.js
+++ src/controller/DataAbsen.js
@@ -3,7 +3,7 @@ const con = require('../Database/database');
 const getDataAbsen = (req,res) => {
     kelas = req.params.kelas
     if (kelas == "sd"){
-        sql = "SELECT absen.tanggal,siswa.nama,siswa.jenis_kelamin,siswa.kelas,absen.status FROM siswa INNER JOIN absen ON siswa.id=absen.id_sidikjari WHERE siswa.kelas < 7 ORDER BY absen.id_absen DESC "
+        sql = "SELECT id_absen,absen.tanggal,siswa.nama,siswa.jenis_kelamin,siswa.kelas,absen.status FROM siswa INNER JOIN absen ON siswa.id=absen.id_sidikjari WHERE siswa.kelas < 7 ORDER BY absen.id_absen DESC "
         con.query(sql,function(err,result){
             if (err) throw err
             res.send({
@@ -12,7 +12,7 @@ const getDataAbsen = (req,res) => {
         })
 
     } else if (kelas == "smp"){
-        sql2 = "SELECT absen.tanggal,siswa.nama,siswa.jenis_kelamin,siswa.kelas,absen.status FROM siswa INNER JOIN absen ON siswa.id=absen.id_sidikjari WHERE siswa.kelas > 6 ORDER BY absen.id_absen DESC "
+        sql2 = "SELECT id_absen,absen.tanggal,siswa.nama,siswa.jenis_kelamin,siswa.kelas,absen.status FROM siswa INNER JOIN absen ON siswa.id=absen.id_sidikjari WHERE siswa.kelas > 6 ORDER BY absen.id_absen DESC "
         con.query(sql2,function(err,result){
             if (err) throw err
             res.send({
@@ -21,7 +21,7 @@ const getDataAbsen = (req,res) => {
             })
         })
     } else if (kelas < 10){
-        sql3 = "SELECT absen.tanggal,siswa.nama,siswa.jenis_kelamin,siswa.kelas,absen.status FROM siswa INNER JOIN absen ON siswa.id=absen.id_sidikjari WHERE siswa.kelas = ?"
+        sql3 = "SELECT id_absen,absen.tanggal,siswa.nama,siswa.jenis_kelamin,siswa.kelas,absen.status FROM siswa INNER JOIN absen ON siswa.id=absen.id_sidikjari WHERE siswa.kelas = ?"
         con.query(sql3,kelas,function(err,result){
             if (err) throw err
             res.send({
